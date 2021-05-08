a4f6162d7cf318367e77e82243dcd554f0c7744a
Jesus <jesustrejo10@gmail.com>
2021-05-08 07:22:17 -0400

    Base login screen.

--- app/src/main/java/com/jesustrejo10/simplerssreader/AuthenticateActivity.kt
+++ app/src/main/java/com/jesustrejo10/simplerssreader/AuthenticateActivity.kt
@@ -3,9 +3,9 @@ package com.jesustrejo10.simplerssreader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AuthenticateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.authenticate_activity)
     }
 }
